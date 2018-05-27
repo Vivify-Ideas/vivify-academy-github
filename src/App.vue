3ae862af017dc6932a685f73a440084a9509f409
volkova229 <aleksandra.volkov229@gmail.com>
2018-05-27 15:26:21 +0200

    Zadatak 2

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,13 @@
 <template>
     <v-ons-page>
-      <v-ons-toolbar>
-        <div class="center">{{ title }}</div>
-        <div class="right">
-          <v-ons-toolbar-button>
-            <v-ons-icon icon="ion-navicon, material: md-menu"></v-ons-icon>
-          </v-ons-toolbar-button>
-        </div>
-      </v-ons-toolbar>
-      <div style="text-align: center; padding-top:10px">Hello World!</div>
-      <p style="text-align: center">
-        <v-ons-button @click="alert">Click Me!</v-ons-button>
-      </p>
+      <app-toolbar />
     </v-ons-page>
 </template>
 <script>
-  export default{
-    data() {
-      return {
-        title: 'My app'
-      };
-    },
-    methods: {
-      alert() {
-        this.$ons.notification.alert('This is an Onsen UI alert notification test.');
-      }
-    }
-  };
+import AppToolbar from './components/AppToolbar.vue'
+export default{
+  components: {
+    AppToolbar
+  }
+}
 </script>
