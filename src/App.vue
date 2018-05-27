345436e2a663bf85f46b2791a609551a42b69399
volkova229 <aleksandra.volkov229@gmail.com>
2018-05-27 17:22:10 +0200

    Zadatak 6

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
     <v-ons-page>
       <app-toolbar />
-      <div class="page">
+      <div class="content">
         <app-search :query.sync="query" placeholder="Search by username"/>
+        <v-ons-list v-if="query">
+          <v-ons-list-header>Repositories of {{ query }}</v-ons-list-header>
+          <v-ons-list-item tappable v-for="repo in repos" :key="repo.id">
+            <div class="left">
+              <img class="list-item__thumbnail" :src="repo.owner.avatar_url">
+            </div>
+            <div class="center">
+              <span class="list-item__title">{{ repo.name }}</span>
+              <span class="list-item__subtitle">{{ repo.description }}</span>
+            </div>
+          </v-ons-list-item>
+        </v-ons-list>
       </div>
     </v-ons-page>
 </template>
@@ -13,7 +25,7 @@ import AppToolbar from './components/AppToolbar.vue'
 import AppSearch from './components/AppSearch.vue'
 import { gitHub } from './services'
 
-export default{
+export default {
   components: {
     AppToolbar,
     AppSearch
@@ -42,9 +54,3 @@ export default{
   }
 }
 </script>
-
-<style scoped>
-  .page {
-    padding: 10px;
-  }
-</style>
