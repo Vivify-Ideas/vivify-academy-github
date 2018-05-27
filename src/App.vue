f9300264101dd9d34af19eea2eee56255f1f5d20
volkova229 <aleksandra.volkov229@gmail.com>
2018-05-27 16:07:49 +0200

    Zadatak 3

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <template>
     <v-ons-page>
       <app-toolbar />
+      <app-search :query.sync="query"/>
     </v-ons-page>
 </template>
 <script>
 import AppToolbar from './components/AppToolbar.vue'
+import AppSearch from './components/AppSearch.vue'
 export default{
   components: {
-    AppToolbar
+    AppToolbar,
+    AppSearch
+  },
+
+  data () {
+    return {
+      query: ''
+    }
   }
 }
 </script>
---
+++ src/components/AppSearch.vue
@@ -0,0 +1,25 @@
+<template>
+  <p class="search">
+    <v-ons-search-input
+      :placeholder="placeholder"
+      style="width: 100%;"
+      :value="query"
+      @input="$emit('update:query', $event.target.value)">
+    </v-ons-search-input>
+  </p>
+</template>
+
+<script>
+export default {
+  props: {
+    placeholder: String,
+    query: String
+  }
+}
+</script>
+
+<style scoped>
+  .search {
+    padding: 10px 20px;
+  }
+</style>
